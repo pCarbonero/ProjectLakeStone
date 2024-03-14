d98fe89ab0bc3be8944a58dd293135b8de637e39
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-03-14 13:43:48 +0100

    Push correctPosition

--- TheLakeAndTheStone/src/programmclasses/Stone.java
+++ TheLakeAndTheStone/src/programmclasses/Stone.java
@@ -2,4 +2,24 @@ package programmclasses;
 
 public class Stone {
 
+	/**
+	 * Method that check if player throws the stone into a valid range
+	 * 
+	 * @param x         x stone's position in the table
+	 * @param y         y stone's position in the table
+	 * @param dimensions Max dimension of axes
+	 * @return Returns if the stone can be thrown or not
+	 */
+	private static boolean correctPosition(int x, int y, int dimensions) {
+		// Variable that registers the throw
+		boolean enter = false;
+		// Check if the stone has been thrown in the axes dimensions
+		if (x >= 0 || x <= dimensions && y >= 0 || y <= dimensions) {
+			// Changes the variable to true
+			enter = true;
+		}
+		// Returns the result of thrown
+		return enter;
+
+	}
 }
