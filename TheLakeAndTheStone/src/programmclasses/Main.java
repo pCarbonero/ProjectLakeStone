7d01901d3fcc8a7cde986c02958c1970b86b8ffd
raulromerap <raul.romera@iesnervion.com>
2024-03-14 13:43:47 +0100

    14/03/2024

--- TheLakeAndTheStone/src/programmclasses/Main.java
+++ TheLakeAndTheStone/src/programmclasses/Main.java
@@ -1,9 +1,28 @@
 package programmclasses;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// We call to Scanner's function and we create a object of it
+		Scanner scan = new Scanner(System.in);
+		
+		// Variable which save the name of the user
+		String name = "";
+		
+		// Print a message of welcome to game
+		System.out.println("Welcome to 'The Lake And The Stone'");
+		
+		// We ask the name to the user
+		System.out.println("What's your name?");
+		// Scan and save the user's name
+		name = scan.nextLine();
+		
+		
+		// Scanner's close
+		scan.close();
 
 	}
 
--- TheLakeAndTheStone/src/programmclasses/Stone.java
+++ TheLakeAndTheStone/src/programmclasses/Stone.java
@@ -2,4 +2,6 @@ package programmclasses;
 
 public class Stone {
 
+	
+	
 }
