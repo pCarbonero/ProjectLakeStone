db113fec1d075269197a830456709f9c358843be
pCarbonero <pablo.carbonero@iesnervion.es>
2024-04-11 12:50:14 +0200

    [new] attributes and constructors

--- TheLakeAndTheStone/src/programmclasses/Stone.java
+++ TheLakeAndTheStone/src/programmclasses/Stone.java
@@ -1,5 +1,33 @@
 package programmclasses;
 
 public class Stone {
+	// tamaño tablero
+		final int SIZE = 8;
+		// array para el lago
+		int[][] lago = new int[SIZE][SIZE];
+		// posición en X
+		int posX = 0;
+		// posición en Y
+		int posY = 0;
+		// intensidad
+		int intensidad;
 
+		/**
+		 * constructor with attributes of the class 
+		 * @param posX
+		 * @param posY
+		 * @param intensity
+		 */
+		public Stone(int posX, int posY, int intensity) {
+			this.posX = posX;
+			this.posY = posY;
+			this.intensidad = intensity;
+		}
+
+		/**
+		 * Constructor without parameters
+		 */
+		public Stone() {
+
+		}
 }
